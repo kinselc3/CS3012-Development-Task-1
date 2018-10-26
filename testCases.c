4b56eaab2f391f894caed839835deddb9f4b956c
kinselc3 <kinselc3@tcd.ie>
2018-10-26 13:07:03 +0100

    added proper Graph class
    Graph class taken from GeeksforGeeks.org
    
    https://www.geeksforgeeks.org/graph-and-its-representations/

--- testCases.c
+++ testCases.c
@@ -1,45 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cbdd.h>
+//#include <cbdd.h>
 
 #include "DAG.c"
 
 
 int main()
 {
-  struct node *src        = newNode(20);
-  src->left               = newNode(8);
-  src->right              = newNode(22);
-  src->left->left         = newNode(4);
-  src->left->right        = newNode(12);
-  src->left->right->left  = newNode(10);
-  src->left->right->right = newNode(14);
-  linkNodes(src->left->right->right,src,0);
-  int n1 = 8, n2 = 22;
-  struct node *t = lca(src, n1, n2);
 
-  it("testing normal scenario", ^{
-        expect_equal(t->data, 20) ;
-  });
-
-  n1 = 8, n2 = 4;
-  t = lca(src, n1, n2);
-  it("testing parent and child", ^{
-        expect_equal(t->data, 8) ;
-  });
-
-  n1 = 12, n2 = 12;
-  t = lca(src, n1, n2);
-  it("testing same node", ^{
-        expect_equal(t->data, 12) ;
-  });
-
-  n1 = 0, n2 = 0;
-  t = lca(src, n1, n2);
-
-  it("testing missing node", ^{
-        expect_null(t) ;
-  });
-
-
+  int V = 5; 
+  struct Graph* graph = createGraph(V); 
+  addEdge(graph, 0, 1); 
+  addEdge(graph, 0, 4); 
+  addEdge(graph, 1, 2); 
+  addEdge(graph, 1, 3); 
+  addEdge(graph, 1, 4); 
+  addEdge(graph, 2, 3); 
+  addEdge(graph, 3, 4); 
+  
+  // print the adjacency list representation of the above graph 
+  printGraph(graph); 
+ 
+ 
 }
